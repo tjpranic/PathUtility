aa554746c3d57bb65e578140ae051efd9769bbe0
Thomas Pranic <tjpranic@gmail.com>
2015-06-25 11:00:10 +1000

    Minor refactoring

--- PathUtility/src/elevator/Shell32X.java
+++ PathUtility/src/elevator/Shell32X.java
@@ -57,7 +57,7 @@ public interface Shell32X extends Shell32 {
     public static class size_t extends IntegerType {
         private static final long serialVersionUID = -6951932265298537496L;
         
-        public size_t( ) { this(0); }
+        public size_t( ) { this( 0 ); }
         public size_t( long value ) { super( Native.SIZE_T_SIZE, value ); }
     }
 
